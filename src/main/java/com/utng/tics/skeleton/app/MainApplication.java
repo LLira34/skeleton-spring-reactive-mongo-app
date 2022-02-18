de9a1351ce2acb371c8c06a6c7af9807ec8c6653
llira34 <luislira1029@gmail.com>
2022-02-18 14:09:12 -0600

    rename classes, add editorconfig and app yml

--- src/main/java/com/utng/tics/skeleton/app/MainApplication.java
+++ src/main/java/com/utng/tics/skeleton/app/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkeletonSpringReactiveMongoAppApplication {
+public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkeletonSpringReactiveMongoAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
 }
