e225056d9e008c6643c6d91a69c98e1f067bb9c2
llira34 <luislira1029@gmail.com>
2022-02-18 14:01:13 -0600

    initial commit

---
+++ src/main/java/com/utng/tics/skeleton/app/SkeletonSpringReactiveMongoAppApplication.java
@@ -0,0 +1,13 @@
+package com.utng.tics.skeleton.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonSpringReactiveMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonSpringReactiveMongoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utng/tics/skeleton/app/SkeletonSpringReactiveMongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utng.tics.skeleton.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonSpringReactiveMongoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
